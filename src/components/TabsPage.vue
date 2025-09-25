544b23d27a60cd72edd6826c404fecc34810a43b
Isabela Pessoal <guimaraeszisabela@gmail.com>
2025-09-25 14:45:56 -0300

    Estilização dos componentes e telas - Imagens adicionadas - Mudança das cores e fonte do app - Ajustes na estilização das telas

--- src/components/TabsPage.vue
+++ src/components/TabsPage.vue
@@ -5,11 +5,11 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="mapa" href="/tabs/mapa">
-          <ion-img src="@/assets/mapa.png" class="tab-icon" />
+          <img src="@/assets/mapa.png" class="tab-icon" />
           <ion-label>Mapa</ion-label>
         </ion-tab-button>
         <ion-tab-button tab="ranking" href="/tabs/ranking">
-          <ion-img src="@/assets/ranking.png" class="tab-icon" />
+          <img src="@/assets/ranking.png" class="tab-icon" />
           <ion-label>Ranking</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -29,3 +29,22 @@ import {
 } from "@ionic/vue";
 import { square, triangle } from "ionicons/icons";
 </script>
+
+<style scoped>
+ion-tab-bar{
+  --background: var(--cinza-medio);
+  border: none;
+}
+
+ion-label{
+  color: var(--preto-carvao);
+  font-size: 15px;
+}
+
+.tab-icon {
+  width: 30px;     
+  height: 30px;
+  object-fit: contain; 
+  margin-bottom: 4px;  
+}
+</style>
