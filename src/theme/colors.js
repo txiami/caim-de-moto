d5f82373d22dcd878d8a570fa451e046e8357729
Isabela Pessoal <guimaraeszisabela@gmail.com>
2025-09-24 22:26:06 -0300

    Adicionando arquivo de cores para estilização consistente

---
+++ src/theme/colors.js
@@ -0,0 +1,9 @@
+
+export const COLORS = {
+  preto_carvao: "#1A1A1A",
+  laranja_vibrante: "#F29F05",
+  branco: "#FFFFFF",
+  vermelho_queimado: "#C14432",
+  azul_petroleo: "#1F3B73",
+  cinza_medio: "#A0A0A0",
+};
